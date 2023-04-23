d0c5e674b94bbb4d2888f05911ed83d8aae746c7
¨Pieter <pieter@pieterrees.nl>
2023-04-23 23:12:26 +0200

    removed color

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,12 +22,14 @@ module.exports = {
       typography: {
         DEFAULT: {
           css: {
-            color: "#333",
             a: {
               color: "#3182ce",
               "&:hover": {
                 color: "#2c5282",
               },
+            },
+            p: {
+              fontSize: "1.5rem",
             },
           },
         },
